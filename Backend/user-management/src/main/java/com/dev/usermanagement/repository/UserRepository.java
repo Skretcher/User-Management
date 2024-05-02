0da5f9005bb07573bf8fb526df899b8b86c1be15
Skretcher <devendra3170@gmail.com>
2024-05-02 13:26:27 +0530

    Basic structure completed but getting error
    user is not a managed class error is creating problem

--- Backend/user-management/src/main/java/com/dev/usermanagement/repository/UserRepository.java
+++ Backend/user-management/src/main/java/com/dev/usermanagement/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.dev.usermanagement.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.dev.usermanagement.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- Backend/user-management/src/main/java/com/dev/usermanagement/service/UserService.java
+++ Backend/user-management/src/main/java/com/dev/usermanagement/service/UserService.java
@@ -1,5 +1,32 @@
 package com.dev.usermanagement.service;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.dev.usermanagement.model.User;
+import com.dev.usermanagement.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
