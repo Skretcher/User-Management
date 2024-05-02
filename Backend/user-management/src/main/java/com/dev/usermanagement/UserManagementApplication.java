b8bbae6a4e77e6a581544eee1ee107a360d48456
Skretcher <devendra3170@gmail.com>
2024-05-02 12:57:26 +0530

    Restarted Working Basic setup completed without any error

--- Backend/user-management/src/main/java/com/dev/usermanagement/UserManagementApplication.java
+++ Backend/user-management/src/main/java/com/dev/usermanagement/UserManagementApplication.java
@@ -2,10 +2,8 @@ package com.dev.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-@EntityScan("com.dev.usermanagement.Entity")
-@SpringBootApplication
 
+@SpringBootApplication
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
